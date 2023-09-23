5d4379bd10f03a3d60ba8883a422cdb30f1a528b
malfiashadath2005 <144474461+malfiashadath2005@users.noreply.github.com>
2023-09-23 17:05:43 +0530

    Add files via upload

--- main (1).py
+++ main (1).py
@@ -1,19 +1,24 @@
-class Player:
-    def play(self):
-        print("The player is playing cricket")
+class Student:
+    def _init_(self, name, roll_number, cgpa):
+        self.name = name
+        self.roll_number = roll_number
+        self.cgpa = cgpa
+def sort_students(student_list):
+    sorted_students = sorted(student_list, key=lambda student: student.cgpa, reverse=True)
+    return sorted_students
 
-class Batsman(Player):
-    def play(self):
-        print("The batsman is batting")
 
-class Bowler(Player):
-    def play(self):
-        print("The bowler is bowling")
+# Sample list of student objects
+students = [
+    Student("Alice", "A101", 3.8),
+    Student("Bob", "B102", 3.9),
+    Student("Charlie", "C103", 3.7),
+    Student("David", "D104", 3.95),
+]
 
-# Create objects of Batsman and Bowler classes
-batsman = Batsman()
-bowler = Bowler()
+# Sort the students by CGPA
+sorted_students = sort_students(students)
 
-# Call the play() method for each object
-batsman.play()
-bowler.play()
+# Print the sorted list of students
+for student in sorted_students:
+    print(f"Name: {student.name}, Roll Number: {student.roll_number}, CGPA: {student.cgpa}")
--- main.py
+++ main.py
@@ -1,37 +1,25 @@
-class BankAccount:
+def linear_search_product(product_list, target_product_name):
+    indices = []
+    for index, product in enumerate(product_list):
+        if product["name"] == target_product_name:
+            indices.append(index)
+    return indices
+# Sample list of products (list of dictionaries)
+products = [
+    {"name": "Laptop", "price": 800},
+    {"name": "Phone", "price": 500},
+    {"name": "Tablet", "price": 300},
+    {"name": "Laptop", "price": 900},
+    {"name": "Laptop", "price": 850},
+]
 
-  def __init__(self, account_number, account_holder, initial_balance=0.0):
-    self.__account_number = account_number
-    self.__account_holder = account_holder
-    self.__balance = initial_balance
+target_product_name = "Laptop"
 
-  def deposit(self, amount):
-    if amount > 0:
-      self.__balance += amount
-      print(f"Deposited ${amount}. New balance: ${self.__balance}")
-    else:
-      print("Invalid deposit amount. Amount must be greater than zero.")
+# Call the linear_search_product function
+result_indices = linear_search_product(products, target_product_name)
 
-  def withdraw(self, amount):
-    if 0 < amount <= self.__balance:
-      self.__balance -= amount
-      print(f"Withdrew ${amount}. New balance: ${self.__balance}")
-    elif amount > self.__balance:
-      print("Insufficient funds. Withdrawal not allowed.")
-    else:
-      print("Invalid withdrawal amount. Amount must be greater than zero.")
-
-  def display_balance(self):
-    print(f"Account Holder: {self.__account_holder}")
-    print(f"Account Number: {self.__account_number}")
-    print(f"Current Balance: ${self.__balance}")
-
-
-# Create an instance of the BankAccount class
-account = BankAccount("12345", "John Doe", 1000.0)
-
-# Test deposit and withdrawal functionality
-account.display_balance()
-account.deposit(500.0)
-account.withdraw(200.0)
-account.display_balance()
+# Print the indices of the target product
+if result_indices:
+    print(f"The target product '{target_product_name}' was found at indices: {result_indices}")
+else:
+    print(f"The target product '{target_product_name}' was not found in the list.")
